fe84e1caf1552a41b2df52313f7c576285b6fc17
94ng6c2829-oss <94ng6c2829@privaterelay.appleid.com>
2026-08-30 20:12:59 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,4 @@
-const C='shihong-ledger-ot-v1-1';
-const A=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){
-    e.respondWith(
-      fetch(e.request)
-        .then(r=>{
-          const copy=r.clone();
-          caches.open(C).then(c=>c.put('./index.html',copy));
-          return r;
-        })
-        .catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-  e.respondWith(
-    caches.match(e.request).then(cached=>{
-      const network=fetch(e.request).then(r=>{
-        const copy=r.clone();
-        caches.open(C).then(c=>c.put(e.request,copy));
-        return r;
-      }).catch(()=>cached);
-      return cached || network;
-    })
-  );
-});
+const C='shihong-v2-1';const A=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{let q=r.clone();caches.open(C).then(c=>c.put('./index.html',q));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{let q=r.clone();caches.open(C).then(c=>c.put(e.request,q));return r})))});
